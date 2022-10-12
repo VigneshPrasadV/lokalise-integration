d0a245af8345b5102fedfd74c0001eedd9abe159
Vignesh Prasad V <vigneshprasad.v@gmail.com>
2022-10-12 14:29:13 +0530

    Create language.js

---
+++ language.js
@@ -0,0 +1,12 @@
+module.exports = {
+  messages: {
+    Hide: "Hide",
+    Show: "Show",
+    "Tax Profile": "Tax Profile",
+    Back: "Back",
+    Cancel: "Cancel",
+    Previous: "Previous",
+    "Save draft": "Save draft",
+    Continue: "Continue"
+    }
+};
